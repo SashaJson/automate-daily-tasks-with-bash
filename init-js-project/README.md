4a0f5ce5228a27941a2860cad76823b4ab730d28
SashaJson <sashamiller666@gmail.com>
2021-04-09 02:13:02 +0300

    added init-js-project

---
+++ init-js-project/README.md
@@ -0,0 +1,1 @@
+For file 'init-js.sh' execute: `chmod u+x init-js.sh`bash
---
+++ init-js-project/init-js.sh
@@ -0,0 +1,5 @@
+echo "Initializing JS project at $(owd)"
+git init
+npm init -y # create package.json with all the defaults
+mkdir scripts
+touch scripts/index.js
